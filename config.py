db207436ec30fb4508f508cf25c7936cb51ad814
guangyong02 <67313893+guangyong02@users.noreply.github.com>
2022-09-22 19:12:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cvodm9afoekj.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "tanmingkit-bucket"
+customregion = "us-east-1"
